6b92faf98d47d729c9a8a9b18c75738dd15b7ba4
Dinush Jayakody <dinushjay@gmail.com>
2020-06-03 00:26:33 +1000

    updated weather with humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.location.name + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.location.name + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.' + 'The humidity is ' + body.current.humidity)
         }
     })
 }
